aecfd6e7cc656ffee56b6eaf83ab2b51201a7b5e
Prathmesh kulkarni <prathmeshkulkarni312@gmail.com>
2025-01-23 16:48:58 +0530

    Updated Manager component for productio10

--- frontend/src/pages/First.jsx
+++ frontend/src/pages/First.jsx
@@ -21,7 +21,7 @@ function First()
         <h1 className="text-7xl mx-auto w-40  "> Login </h1>
 
         <hr></hr>
-        <h1 className="text-4xl lg:mx-auto w-96 lg:pl-20 text-red-600 m-14"> Select User Type </h1>
+        <h1 className="text-4xl lg:mx-auto w-96 lg:pl-20 text-red-600 m-12"> Select User Type </h1>
 
         <div className="lg:flex lg:space-x-20 lg:ml-36 sm:flex-row sm:pl-8 space-y-14 pl-14">
   {/* Your content goes here */}
