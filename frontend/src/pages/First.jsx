739d191c9b812baf6f2678a0515e91c510f9dd89
Prathmesh kulkarni <prathmeshkulkarni312@gmail.com>
2025-01-23 15:47:06 +0530

    Updated Manager component for productio5

--- frontend/src/pages/First.jsx
+++ frontend/src/pages/First.jsx
@@ -14,7 +14,7 @@ function First()
       }
       
     return(
-        <><div className="mx-auto  space-y-3 p-9">
+        <><div className="mx-auto  space-y-3 p-9 sm:p-1">
             <button  onClick={next} className="text-white absolute top-0 right-0 mr-10 mt-10 font-bold bg-blue-700 rounded-lg p-3 ml-28 mx-auto">
                 signup 
               </button>
@@ -23,15 +23,16 @@ function First()
         <hr></hr>
         <h1 className="text-4xl mx-auto w-96 pl-20 text-red-600"> Select User Type </h1>
 
-        <div className="flex space-x-20 ml-36">
+        <div className="lg:flex lg:space-x-20 lg:ml-36 sm:flex-row sm:ml-8">
+  {/* Your content goes here */}
         <p ><button onClick={() => handle("Manager")} className=" hover:shadow-2xl " >
             <FcManager size={270} />
             <h1 className="mx-auto text-2xl w-28">Manager</h1>
              </button>
         </p>
-        <p ><button onClick={() => handle("Pantry Staff")} className=" hover:shadow-2xl">
-            <img src={image}  className="h-60"/> 
-            <h1 className="mx-auto text-2xl w-28 p-3 text-center">Pantry Staff</h1> 
+        <p className=" "><button onClick={() => handle("Pantry Staff")} className=" mx-auto  hover:shadow-2xl">
+            <img src={image}  className="h-60 mx-auto"/> 
+            <h1 className="mx-auto text-2xl w-28 p-3 text-center sm:ml-28">Pantry Staff</h1> 
             </button>
         </p>
         <p ><button onClick={() => handle("Delivery Personnel")} className=" hover:shadow-2xl">
