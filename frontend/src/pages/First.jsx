5f7e487a6235901b5b155dc344e6a42fa9aede4f
Prathmesh kulkarni <prathmeshkulkarni312@gmail.com>
2025-01-23 16:36:22 +0530

    Updated Manager component for productio7

--- frontend/src/pages/First.jsx
+++ frontend/src/pages/First.jsx
@@ -23,7 +23,7 @@ function First()
         <hr></hr>
         <h1 className="text-4xl lg:mx-auto w-96 lg:pl-20 text-red-600 m-20"> Select User Type </h1>
 
-        <div className="lg:flex lg:space-x-20 lg:ml-36 sm:flex-row sm:pl-8">
+        <div className="lg:flex lg:space-x-20 lg:ml-36 sm:flex-row sm:pl-8 space-y-10">
   {/* Your content goes here */}
         <p ><button onClick={() => handle("Manager")} className=" hover:shadow-2xl " >
             <FcManager size={270} />
